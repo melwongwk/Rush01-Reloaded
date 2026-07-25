eb126f049fb3ab89d0bd010f1480bf9b573d317f
Melvin <melvinwk.wong@gmail.com>
2026-07-25 15:42:30 +0800

    refactored rush01.h

--- ex00/rush01.h
+++ ex00/rush01.h
@@ -15,28 +15,34 @@ typedef struct s_game
 }	t_game;
 
 /* utils.c */
+
 bool	ft_isspace(char c);
 bool	ft_isdigit(char c);
 int		ft_strlen(char *str);
 
 /* input.c */
+
 int		parse_input(char *str, t_game *game);
 
 /* validation.c */
+
 int		check_row(t_game *game, int row);
 int		check_col(t_game *game, int col);
 
 /* clues.c */
+
 int		count_from_left(t_game *game, int row);
 int		count_from_right(t_game *game, int row);
 int		count_from_top(t_game *game, int col);
 int		count_from_bottom(t_game *game, int col);
 
 /* solver.c */
+
 int		can_place(t_game *game, int row, int col, int num);
 int		solve(t_game *game);
 
 /* output.c */
+
 void	print_grid(t_game *game);
 void	print_error(void);
 
