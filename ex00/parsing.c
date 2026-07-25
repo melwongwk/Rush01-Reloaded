a59c3d82914bd3372635b3e1795483a342cf4478
Mei Keng Chan <meichan@u81z02s01.42kl.edu.my>
2026-07-25 23:04:58 +0800

    Completed function parse_input and print_error.

--- ex00/parsing.c
+++ ex00/parsing.c
@@ -1,1 +1,37 @@
-test
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meichan <meichan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/25 17:01:43 by meichan           #+#    #+#             */
+/*   Updated: 2026/07/25 23:02:42 by meichan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	parse_input(char *str, t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i <= 15 && j <= 31)
+	{
+		clues[i] = str[j];
+		i++;
+		j += 2;
+	}
+	return (0);
+}
+
+void	print_grid(t_game *game)
+{
+	grid[][];
+}
+
+void	print_error(void)
+{
+	write (1, "Error\n", 6);
+}
